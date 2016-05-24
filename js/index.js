937f76cc59d28a68df4920f57e4a7e45046875c3
Joshua Henson <joshuahenson@users.noreply.github.com>
2016-05-24 15:18:09 -0700

    Converted api to nodes/links with rough styling

--- js/index.js
+++ js/index.js
@@ -14,69 +14,75 @@ const svg = d3.select('#root').append('svg')
 
 const defs = svg.append('defs');
 
-// d3.json('data.json', (error, data) => {
-//   if (error) console.error(error);
+d3.json('../json/data.json', (error, data) => {
+  if (error) console.error(error);
+  console.log(data.pubAvatar);
+  const authorIndex = {};
+  const nodes = [{ author: 'Free Code Camp', avatar: data.pubAvatar, value: 50 }];
+  const links = [];
 
-// todo convert raw json api into usable nodes/links
+  // build nodes and links
+  data.articles.forEach((d) => {
+    const nodesLength = nodes.length;
+    if (d.author in authorIndex) {
+      nodes[authorIndex[d.author]].value += 1;
+      links.push({ source: nodesLength, target: authorIndex[d.author] });
+    } else {
+      authorIndex[d.author] = nodesLength;
+      nodes.push({ author: d.author, avatar: d.avatar, value: 1 });
+      links.push({ source: nodesLength, target: 0 });
+      links.push({ source: nodesLength + 1, target: nodesLength });
+    }
+    nodes.push({ author: d.author, title: d.title, url: d.url });
+  });
 
-const graph = {
-  nodes: [
-    { author: 'fcc', value: 50, avatar: 'https://cdn-images-2.medium.com/fit/c/36/36/1*MotlWcSa2n6FrOx3ul89kw.png' },
-    { author: 'q', value: 5, avatar: 'https://cdn-images-2.medium.com/fit/c/36/36/1*CEi35YFI3DqbuAu0jAhFYA.jpeg' },
-    { author: 'q', article: 'test' },
-  ],
-  links: [
-    { source: 1, target: 0 },
-    { source: 2, target: 1 },
-  ]
-};
+  force
+    .nodes(nodes)
+    .links(links)
+    .start();
 
-force
-  .nodes(graph.nodes)
-  .links(graph.links)
-  .start();
+  const link = svg.selectAll('.link')
+      .data(links)
+    .enter().append('line')
+      .attr('class', 'link');
 
-const link = svg.selectAll('.link')
-    .data(graph.links)
-  .enter().append('line')
-    .attr('class', 'link');
+  const node = svg.selectAll('.node')
+      .data(nodes)
+    .enter().append('g')
+      .attr('class', 'node')
+      .call(force.drag);
 
-const node = svg.selectAll('.node')
-    .data(graph.nodes)
-  .enter().append('g')
-    .attr('class', 'node')
-    .call(force.drag);
-
-// create patterns to fill circles with avatars
-graph.nodes.filter(d => d.avatar).forEach(d => {
-  const r = Math.pow(d.value * 125, 1 / 3);
-  const size = r * 2;
-  defs.append('pattern')
-    .attr('id', d.author)
-    .attr('patternUnits', 'userSpaceOnUse')
-    .attr('width', size)
-    .attr('height', size)
-    .attr('x', -r)
-    .attr('y', -r)
-    .append('image')
-      .attr('xlink:href', d.avatar)
+  // create patterns to fill circles with avatars
+  nodes.filter(d => d.avatar).forEach(d => {
+    console.log(d);
+    const r = Math.pow(d.value * 325, 1 / 3);
+    const size = r * 2;
+    defs.append('pattern')
+      .attr('id', d.author.replace(/ /g, '_'))
+      .attr('patternUnits', 'userSpaceOnUse')
       .attr('width', size)
       .attr('height', size)
-      .attr('x', 0)
-      .attr('y', 0);
-});
+      .attr('x', -r)
+      .attr('y', -r)
+      .append('image')
+        .attr('xlink:href', d.avatar)
+        .attr('width', size)
+        .attr('height', size)
+        .attr('x', 0)
+        .attr('y', 0);
+  });
 
-// append circle filled with image pattern for avatars or color
-node.append('circle')
-  .attr('r', d => Math.pow(d.value * 125, 1 / 3) || 5)
-  .style('fill', d => (d.avatar ? `url(#${d.author})` : '#006400'));
+  // append circle filled with image pattern for avatars or color
+  node.append('circle')
+    .attr('r', d => Math.pow(d.value * 325, 1 / 3) || 5)
+    .style('fill', d => (d.avatar ? `url(#${d.author.replace(/ /g, '_')})` : '#006400'));
 
-force.on('tick', () => {
-  link.attr('x1', d => d.source.x)
-        .attr('y1', d => d.source.y)
-        .attr('x2', d => d.target.x)
-        .attr('y2', d => d.target.y);
+  force.on('tick', () => {
+    link.attr('x1', d => d.source.x)
+          .attr('y1', d => d.source.y)
+          .attr('x2', d => d.target.x)
+          .attr('y2', d => d.target.y);
 
-  node.attr('transform', d => `translate(${d.x}, ${d.y})`);
-});
-// }); // end json
+    node.attr('transform', d => `translate(${d.x}, ${d.y})`);
+  });
+}); // end d3.json
